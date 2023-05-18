10095a6c5079ab2eaeb13ba77a490be17d20c39b
David Pérez <dperezz@unal.edu.co>
2023-05-17 19:37:19 -0500

    Updated: changes to the main page and another endpoint

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__)  # Create a Flask instance.
 
 @app.route("/")
 def home():
-    return "<p>Hello world!</p>"
+    """Returns the main app page."""
+    
+    return render_template("index.html")
+
+@app.route("/surprise")
+def home():
+    """Returns tan easter egg page."""
+
+    return '<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Click me for an easter egg ;)</a>'
